667a6ef6dafda0335aa67194abb8871f9ea4765c
Sho Hashimoto <hashimoto@shokai.org>
2013-04-10 17:53:07 +0900

    simplify

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,5 @@
 io = Sinatra::RocketIO
 
-io.on :connect do |client|
-  puts "new client : #{client}"
-end
-
-io.on :disconnect do |client|
-  puts "bye : #{client}"
-end
-
-
 ## receive from client
 io.on :hello do |message, client|
   puts "> receive #{message} from #{client}"
