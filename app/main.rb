d2dea5ac87ceb3e559a37dc101e18bdf2fda5c6a
Sho Hashimoto <hashimoto@shokai.org>
2013-10-02 01:44:42 +0900

    update for RocketIO v0.3.0

--- app/main.rb
+++ app/main.rb
@@ -2,7 +2,7 @@ io = Sinatra::RocketIO
 
 ## receive "hello" from client
 io.on :hello do |message, client|
-  puts "> receive '#{message}' from #{client.session}"
+  puts "> receive '#{message}' from #{client.session} (#{client.type} #{client.address})"
 
   ## push "echo" to client
   io.push :echo, message
